8cdbcc431a05f2426359e5b15a5b257194413957
AndreyOksenyuk <oksenyuk.andrey@gmail.com>
2020-08-17 23:09:30 +0300

    Updates

---
+++ precache-manifest.d572dd5059f7aec2bfcc9ab0b2ceb685.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0462e83f866ba05e314f4c6ee13ac7d4",
+    "url": "/Shophia-react/index.html"
+  },
+  {
+    "revision": "3f4c83a3ba92093ae2ad",
+    "url": "/Shophia-react/static/css/2.f15748dc.chunk.css"
+  },
+  {
+    "revision": "bfdaa32ef0a8c5626898",
+    "url": "/Shophia-react/static/css/main.67e73c58.chunk.css"
+  },
+  {
+    "revision": "3f4c83a3ba92093ae2ad",
+    "url": "/Shophia-react/static/js/2.5d345b67.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Shophia-react/static/js/2.5d345b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfdaa32ef0a8c5626898",
+    "url": "/Shophia-react/static/js/main.dc8f09ac.chunk.js"
+  },
+  {
+    "revision": "06e63adf69c97c1f3f99",
+    "url": "/Shophia-react/static/js/runtime-main.eaa37088.js"
+  },
+  {
+    "revision": "2a623824e13ccf2cce476017d9c318ed",
+    "url": "/Shophia-react/static/media/1.2a623824.jpg"
+  },
+  {
+    "revision": "874adf531b609b863a6253582d6fa7fd",
+    "url": "/Shophia-react/static/media/1.874adf53.jpg"
+  },
+  {
+    "revision": "9db5a286eb46424c35354b23b5949acf",
+    "url": "/Shophia-react/static/media/1.9db5a286.jpg"
+  },
+  {
+    "revision": "5eedef7f2283b0f972d5c47869f06677",
+    "url": "/Shophia-react/static/media/2.5eedef7f.jpg"
+  },
+  {
+    "revision": "b929765dafb1c24fe3b8268141aae50d",
+    "url": "/Shophia-react/static/media/2.b929765d.jpg"
+  },
+  {
+    "revision": "3167e7492938d776d03b5a7d187bdc25",
+    "url": "/Shophia-react/static/media/3.3167e749.jpg"
+  },
+  {
+    "revision": "613fcf7d7e40f500e3b8fb168a2d5735",
+    "url": "/Shophia-react/static/media/3.613fcf7d.jpg"
+  },
+  {
+    "revision": "8fcb31a3a499622199dd33b95f3b5b14",
+    "url": "/Shophia-react/static/media/4.8fcb31a3.jpg"
+  },
+  {
+    "revision": "d2b7ac44b375f365f02ec41e2f7c3b34",
+    "url": "/Shophia-react/static/media/4.d2b7ac44.jpg"
+  },
+  {
+    "revision": "d2c8fa24f01ced42316ac82169ea1895",
+    "url": "/Shophia-react/static/media/5.d2c8fa24.jpg"
+  },
+  {
+    "revision": "833bcf2a6514a7ed4c8cf3ac99dd57b4",
+    "url": "/Shophia-react/static/media/6.833bcf2a.jpg"
+  },
+  {
+    "revision": "8a4eb7afd2c4a295058d166cec4fa121",
+    "url": "/Shophia-react/static/media/7.8a4eb7af.jpg"
+  },
+  {
+    "revision": "12a68d2b32b31cc05ec58a99685d2545",
+    "url": "/Shophia-react/static/media/8.12a68d2b.jpg"
+  },
+  {
+    "revision": "4027bc4528c3803a7b4fdfe1ef155df7",
+    "url": "/Shophia-react/static/media/KrinkesDecorPERSONAL.4027bc45.ttf"
+  },
+  {
+    "revision": "77b6257e2b5319fe8cce74fd19342d14",
+    "url": "/Shophia-react/static/media/Layer-12.77b6257e.png"
+  },
+  {
+    "revision": "80f3d7832b6bffc75faa74c03a1f3896",
+    "url": "/Shophia-react/static/media/Layer-13.80f3d783.png"
+  },
+  {
+    "revision": "5366d31d147601def1136698e8bf5520",
+    "url": "/Shophia-react/static/media/center.5366d31d.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Shophia-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Shophia-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Shophia-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Shophia-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Shophia-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
